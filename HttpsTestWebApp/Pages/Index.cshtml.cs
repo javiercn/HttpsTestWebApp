0ef8beeb027c16e5aa55095645b0915a7cd5f999
jacalvar <jacalvar@microsoft.com>
2022-05-25 17:15:25 +0200

    Add project files.

---
+++ HttpsTestWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HttpsTestWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public WeatherForecast[] Forecasts { get; private set; } = Array.Empty<WeatherForecast>();
+
+        public async Task OnGet()
+        {
+            using var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://localhost:7077")
+            };
+
+            Forecasts = (await client.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast"))!;
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
